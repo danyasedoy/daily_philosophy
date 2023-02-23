a236f01eabf05d289b085edff931b0fd5afb9c79
Danila_Mochalov <volzhankaonelove@yandex.ru>
2023-02-23 22:09:43 +0400

    Добавлены сохранение и загрузка темы приложения из кэша

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:nirs/navigation/navigation.dart';
 import 'package:nirs/themes/themes.dart';
-
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 
 void main() {
@@ -20,17 +20,37 @@ class MyApp extends StatefulWidget {
 class MyAppState extends State<MyApp> {
 
   ThemeData theme = createDarkTheme();
+  final _storage = const FlutterSecureStorage();
 
   void changeThemeData() {
     if (theme == createDarkTheme()) {
       theme = createLightTheme();
+      _storage.write(key: 'theme', value: 'light');
       setState(() {
       });
     }
     else {
       theme = createDarkTheme();
+      _storage.write(key: 'theme', value: 'dark');
       setState(() {
       });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    checkForSavedTheme();
+  }
+
+  checkForSavedTheme() async{
+    String? loadedTheme = await _storage.read(key: 'theme');
+    if (loadedTheme != null) {
+      if (loadedTheme == 'dark') {
+        theme = createDarkTheme();
+      } else {
+        theme = createLightTheme();
+      }
     }
   }
 
